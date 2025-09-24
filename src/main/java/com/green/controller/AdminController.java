9cc8e2648ddd63a27d5ea2e98146b1cb9eefeabb
HeoTaehoon7 <greenclass.hoon@gmail.com>
2025-09-24 12:50:32 +0900

    Controller 작업

---
+++ src/main/java/com/green/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.green.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+
+	@GetMapping("/admin")
+	public  String  admin() {
+		return  "Admin Controller";
+	}
+	
+	
+}
