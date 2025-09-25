d66a28a13417a66c307322a278341ba725662777
HeoTaehoon7 <greenclass.hoon@gmail.com>
2025-09-25 12:24:20 +0900

    로그인 필터 구현

--- src/main/java/com/green/demo/config/SecurityConfig.java
+++ src/main/java/com/green/demo/config/SecurityConfig.java
@@ -2,17 +2,36 @@ package com.green.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.green.demo.jwt.LoginFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+	private AuthenticationConfiguration authenticationConfiguration;
+	public SecurityConfig(
+			AuthenticationConfiguration authenticationConfiguration) {
+		this.authenticationConfiguration = authenticationConfiguration;
+	}
+
+	// 2. authenticationManager Bean 등록
+	@Bean
+	public  AuthenticationManager  authenticationManager (
+		AuthenticationConfiguration  configuration	
+			) throws Exception {		
+		return configuration.getAuthenticationManager();
+	}
+	
 	//  인가 작업설정
 	@Bean
 	public  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -30,6 +49,12 @@ public class SecurityConfig {
 	    	   .requestMatchers("/login", "/", "/join").permitAll()  
 	    	   .requestMatchers("/admin").hasRole("ADMIN")
 	    	   .anyRequest().authenticated() )
+	      
+	      // 1. 커스텀 로그인 필터 등록 LoginFilter 필터 추가
+	      // Before, After, At(기존대체)	      
+	      .addFilterAt( new LoginFilter(
+	    		  authenticationManager( authenticationConfiguration ) ), 
+	    	 UsernamePasswordAuthenticationFilter.class)     
 	      
 	      // 세션 설정 JWT 는 STATELESS , 중요 
 	      .sessionManagement( (session) -> session
